2efbe64d28bf36e7694f11ea69ff5ee14dbaffc4
falk <frabatnun97@gmail.com>
2019-10-21 21:25:06 +0200

    Complete part b
    Search by brand

--- practice_2/main.py
+++ practice_2/main.py
@@ -1,6 +1,9 @@
 from practice_2 import database as db
 from practice_2 import ui_options as ui
+from practice_2 import practica
 
 # ui.almacenar_productos()
 # ui.todas_las_marcas()
-ui.buscar_ofertas()
+# ui.buscar_ofertas()
+
+practica.inicia()
--- practice_2/ui_options.py
+++ practice_2/ui_options.py
@@ -1,39 +1,46 @@
 from practice_2 import database as db
-from bs4 import BeautifulSoup
-import tkinter as tk
+from tkinter import messagebox
 
 
 def almacenar_productos():
-    conn = db.create_database()
-    cursor = conn.cursor()
+    try:
+        var = 'Base de datos creada correctamente'
+        conn = db.create_database()
+        cursor = conn.cursor()
 
-    datablock = db.get_soup().find_all('div', class_='js-product-grid-grid')
-    for data in datablock:
-        marca = data.find('h4', class_="product-item__brand").text.strip()
-        print(marca)
-        nombre = data.find('img', class_='lazy')['alt']
-        print(nombre)
-        link = data.find('a', class_='js-article-link')['href']
-        print('https://www.ulabox.com' + link)
-        try:
-            precio_normal = data.find('del', class_='product-item__price--old').text
-        except:
-            precio_normal = data.find('strong', class_='product-item__price').text
-        print(precio_normal)
-        try:
-            precio_oferta = data.find('strong', class_='product-grid-footer__price--new').text
-        except:
-            precio_oferta = '-'
-        print(precio_oferta)
-        precio_kilo = data.find('small', class_='product-item__ppu nano').text.split('|')[1].strip()
-        print(precio_kilo)
-        print()
+        datablock = db.get_soup().find_all('div', class_='js-product-grid-grid')
+        for data in datablock:
+            marca = data.find('h4', class_="product-item__brand").text.strip()
+            print(marca)
+            nombre = data.find('img', class_='lazy')['alt']
+            print(nombre)
+            link = data.find('a', class_='js-article-link')['href']
+            print('https://www.ulabox.com' + link)
+            try:
+                precio_normal = data.find('del', class_='product-item__price--old').text
+            except:
+                precio_normal = data.find('strong', class_='product-item__price').text
+            print(precio_normal)
+            try:
+                precio_oferta = data.find('strong', class_='product-grid-footer__price--new').text
+            except:
+                precio_oferta = '-'
+            print(precio_oferta)
+            precio_kilo = data.find('small', class_='product-item__ppu nano').text.split('|')[1].strip()
+            print(precio_kilo)
+            print()
 
-        cursor.execute('INSERT INTO productos VALUES(?,?,?,?,?,?)',
-                       (marca, nombre, link, precio_normal, precio_oferta, precio_kilo))
-        conn.commit()
+            cursor.execute('INSERT INTO productos VALUES(?,?,?,?,?,?)',
+                           (marca, nombre, link, precio_normal, precio_oferta, precio_kilo))
+            conn.commit()
 
-    db.desconnect(conn)
+        db.desconnect(conn)
+
+    except ValueError as e:
+        var = 'Error al crear la base de datos'
+        print(e)
+
+    messagebox.showinfo('Base de datos', var)
 
 
 def todas_las_marcas():
@@ -46,16 +53,30 @@ def todas_las_marcas():
     print(marcas)
 
     db.desconnect(conn)
+    return marcas
 
 
 def buscar_ofertas():
     conn = db.connect()
     cursor = conn.cursor()
 
-    cursor.execute('''SELECT nombre FROM productos WHERE precio_oferta != "-"''')
+    cursor.execute('SELECT nombre FROM productos WHERE precio_oferta != "-"')
     ofertas = cursor.fetchall()
     ofertas = sorted(set(ofertas))
     print(ofertas)
 
     db.desconnect(conn)
+    return ofertas
 
+
+def busqueda_marcas(marca):
+    conn = db.connect()
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT nombre FROM productos WHERE marca == ?', (marca,))
+    nombres = cursor.fetchall()
+    nombres = sorted(set(nombres))
+    print(nombres)
+
+    db.desconnect(conn)
+    return nombres
