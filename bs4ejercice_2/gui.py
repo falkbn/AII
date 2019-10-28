82dc472e0654e816f492246c3bc16982e2a961a3
falk <frabatnun97@gmail.com>
2019-10-28 11:56:22 +0100

    Add second part of the exercise
    Search types. Remaining: more pages scrapped

--- bs4ejercice_2/gui.py
+++ bs4ejercice_2/gui.py
@@ -13,12 +13,58 @@ def inicia():
     menubar.add_cascade(label="Datos", menu=data_menu)
 
     search_menu = tk.Menu(menubar, tearoff=0)
-    search_menu.add_command(label="Autor", command=None)
-    search_menu.add_command(label="Fecha", command=None)
+    search_menu.add_command(label="Autor", command=buscar_autor)
+    search_menu.add_command(label="Fecha", command=buscar_fecha)
     menubar.add_cascade(label="Buscar", menu=search_menu)
 
     root.config(menu=menubar)
     root.mainloop()
+
+
+def buscar_fecha():
+    window1 = tk.Tk()
+    label_fecha = tk.Label(window1, text='Fecha de búsqueda: ')
+    label_fecha.grid(row=0, column=0)
+    entry1 = tk.Entry(window1, bd=5)
+    entry1.grid(row=0, column=1)
+    button1 = tk.Button(window1, text='Buscar', command=lambda: filtrar_fechas(entry1.get()))
+    button1.grid(row=0, column=2)
+    window1.mainloop()
+
+
+def filtrar_fechas(fecha):
+    window1 = tk.Tk()
+    scroll = tk.Scrollbar(window1)
+    scroll.pack(side='right', fill='y')
+    lb = tk.Listbox(window1, yscrollcommand=scroll.set)
+
+    for id, i in enumerate(m.get_noticia_fecha(fecha)):
+        lb.insert(id, i)
+        lb.pack(fill='both', expand=True)
+
+    window1.mainloop()
+
+
+def buscar_autor():
+    window1 = tk.Tk()
+    spin = tk.Spinbox(window1, values=m.get_autores())
+    spin.grid(row=0, column=0)
+    aceptar = tk.Button(window1, text='Aceptar', command=lambda: filtrar_autores(spin.get()))
+    aceptar.grid(row=1, column=0)
+    window1.mainloop()
+
+
+def filtrar_autores(autor):
+    window1 = tk.Tk()
+    scroll = tk.Scrollbar(window1)
+    scroll.pack(side='right', fill='y')
+    lb = tk.Listbox(window1, yscrollcommand=scroll.set)
+
+    for id, i in enumerate(m.get_noticia_autor(autor)):
+        lb.insert(id, i)
+        lb.pack(fill='both', expand=True)
+
+    window1.mainloop()
 
 
 def datos_mostrar():
--- bs4ejercice_2/method.py
+++ bs4ejercice_2/method.py
@@ -47,13 +47,34 @@ def datos_mostrar():
     return [x[1] + ': ' + x[0] + ' || ' + x[2] for x in noticias]
 
 
-def busq_autor():
+def get_autores():
     conn = db.connect()
     cursor = conn.cursor()
     cursor.execute('SELECT autor FROM noticias')
     autores = cursor.fetchall()
+    autores = sorted((set(autores)))
     return [x[0] for x in autores]
 
 
-def busq_fecha():
-    None
+def get_noticia_autor(autor):
+    conn = db.connect()
+    cursor = conn.cursor()
+    cursor.execute('SELECT titulo, autor, fecha FROM noticias WHERE noticias.autor = ?', (autor,))
+    noticias = cursor.fetchall()
+    return [x[1] + ': ' + x[0] + ' ' + x[2] for x in noticias]
+
+
+def get_fechas():
+    conn = db.connect()
+    cursor = conn.cursor()
+    cursor.execute('SELECT fecha FROM noticias')
+    autores = cursor.fetchall()
+    return [x for x in autores]
+
+
+def get_noticia_fecha(fecha):
+    conn = db.connect()
+    cursor = conn.cursor()
+    cursor.execute('SELECT titulo, autor, fecha FROM noticias WHERE noticias.fecha LIKE ?', ('%' + fecha + '%',))
+    noticias = cursor.fetchall()
+    return [x[1] + ': ' + x[0] + ' ' + x[2] for x in noticias]
