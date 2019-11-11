adfa462bc827c4b3e1c17757bd6062b2b2f27888
falk <frabatnun97@gmail.com>
2019-11-11 19:43:20 +0100

    Add final method
    Session completed

--- bs4ejercice_2/method.py
+++ bs4ejercice_2/method.py
@@ -8,29 +8,9 @@ def datos_cargar():
     cursor = conn.cursor()
 
     try:
-        datablock = db.get_soup().find_all('div', class_='news-body')
-        for data in datablock:
-            titulo = data.find('div', class_='center-content').h2.text.strip()
-            print(titulo)
-            enlace = data.find('div', class_='center-content').a['href']
-            print(enlace)
-            autor = data.find('div', class_='news-submitted').find_all('a')[1].text
-            print(autor)
-            fecha = data.find_all('span', class_='ts visible')[1]['data-ts']
-            fecha = str(datetime.fromtimestamp(int(fecha)))
-            print(fecha)
-            contenido = data.find('div', class_='news-content').text
-            print(contenido)
-            print()
 
-            cursor.execute('INSERT INTO noticias VALUES(?,?,?,?,?)',
-                           (titulo, enlace, autor, fecha, contenido))
-            conn.commit()
-            cursor.execute('SELECT COUNT(*) FROM noticias')
-            c = cursor.fetchone()
 
-        db.disconnect(conn)
-        var = 'Base de datos creada correctamente con ' + str(c[0]) + ' entradas'
+
 
     except ValueError as e:
         var = 'Error al crear la base de datos'
