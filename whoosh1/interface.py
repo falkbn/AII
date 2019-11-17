c7a335b562c833b7d6bd399a5517587849fb4395
falk <frabatnun97@gmail.com>
2019-11-17 20:36:01 +0100

    Add a) from Whoosh Practice 1
    Bs4 Scraping + Whoosh Index creation

--- whoosh1/interface.py
+++ whoosh1/interface.py
@@ -1,49 +1,21 @@
+from whoosh1 import method as m
 import tkinter as tk
-
-from whoosh1 import interface as hud
 
 
 def inicia():
     root = tk.Tk()
-    almacenar = tk.Button(root, text='Almacenar', command=ui.almacenar_productos)
-    almacenar.grid(row=0, column=0)
-    mostrar = tk.Button(root, text='Mostrar', command=mostrar_marcas)
-    mostrar.grid(row=0, column=1)
-    buscar = tk.Button(root, text='Buscar', command=mostrar_ofertas)
-    buscar.grid(row=0, column=2)
+    menubar = tk.Menu(root)
+    data_menu = tk.Menu(menubar, tearoff=0)
+    data_menu.add_command(label="Cargar", command=m.almacenar_noticias)
+    data_menu.add_separator()
+    data_menu.add_command(label="Salir", command=root.quit)
+    menubar.add_cascade(label="Datos", menu=data_menu)
+
+    search_menu = tk.Menu(menubar, tearoff=0)
+    search_menu.add_command(label="Titulo y Descripcion", command=None)
+    search_menu.add_command(label="Fecha", command=None)
+    search_menu.add_command(label="Descripcion", command=None)
+    menubar.add_cascade(label="Buscar", menu=search_menu)
+
+    root.config(menu=menubar)
     root.mainloop()
-
-
-def mostrar_ofertas():
-    window1 = tk.Tk()
-    scroll = tk.Scrollbar(window1)
-    scroll.pack(side='right', fill='y')
-    lb = tk.Listbox(window1, yscrollcommand=scroll.set)
-
-    for id, i in enumerate(ui.buscar_ofertas()):
-        lb.insert(id, i)
-        lb.pack(fill='both', expand=True)
-
-    window1.mainloop()
-
-
-def mostrar_marcas():
-    window1 = tk.Tk()
-    spin = tk.Spinbox(window1, values=ui.todas_las_marcas())
-    spin.grid(row=0, column=0)
-    aceptar = tk.Button(window1, text='Aceptar', command=lambda: filtrar_marcas(spin.get()))
-    aceptar.grid(row=1, column=0)
-    window1.mainloop()
-
-
-def filtrar_marcas(marca):
-    window1 = tk.Tk()
-    scroll = tk.Scrollbar(window1)
-    scroll.pack(side='right', fill='y')
-    lb = tk.Listbox(window1, yscrollcommand=scroll.set)
-
-    for id, i in enumerate(ui.busqueda_marcas(marca)):
-        lb.insert(id, i)
-        lb.pack(fill='both', expand=True)
-
-    window1.mainloop()
